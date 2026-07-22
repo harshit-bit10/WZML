ab1e53c069209b7aaf7ed0e7026433d089361593
Harshit Papnai <109902163+harshit-bit10@users.noreply.github.com>
2026-07-22 15:41:12 +0530

    Update config.py to remove quotes from IDs
    Removed quotes from OWNER_ID and TELEGRAM_API for better formatting.
    
    Signed-off-by: Harshit Papnai  <109902163+harshit-bit10@users.noreply.github.com>

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # REQUIRED CONFIG
 BOT_TOKEN = ""
-OWNER_ID = "6066102279"                               # Require restart after changing it while bot running
-TELEGRAM_API = "26378339"                           # Require restart after changing it while bot running
+OWNER_ID = 6066102279                               # Require restart after changing it while bot running
+TELEGRAM_API = 26378339                           # Require restart after changing it while bot running
 TELEGRAM_HASH = "d5b14fef7f38588836b1a8ba762f8f08"                          # Require restart after changing it while bot running
 DATABASE_URL = "mongodb+srv://sharku:zKKosLfBvResoqhF@cluster0.jped6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_NAME = "SHARKWZMLDB"
